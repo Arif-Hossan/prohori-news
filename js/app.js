7179bf39e7560f955f3b1fb97e77f295bded8e77
Arif Hossan <arif.hossan.eu@gmail.com>
2023-02-18 00:16:31 +0600

    implement the news rating portion

--- js/app.js
+++ js/app.js
@@ -1,56 +1,72 @@
-const loadCategories = async () =>{
-    const url = `https://openapi.programming-hero.com/api/news/categories`;
-    const res = await fetch(url);
-    const data =await res.json();
-    displayCategories(data.data.news_category);
+const loadCategories = async () => {
+  const url = `https://openapi.programming-hero.com/api/news/categories`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayCategories(data.data.news_category);
 };
 // display categories
-const displayCategories = categories =>{
-    const categoriesContainer = document.getElementById('categories');
-    categories.forEach(category => {
-    const categoryItem = document.createElement('li');
-    categoryItem.classList.add('navbar-items','me-2', 'mb-2');
-    categoryItem.innerHTML=
-    `<a class="nav-link" onclick="loadCategoryNews('${category.category_id}')">${category.category_name}</a>`;;
+const displayCategories = (categories) => {
+  const categoriesContainer = document.getElementById("categories");
+  categories.forEach((category) => {
+    const categoryItem = document.createElement("li");
+    categoryItem.classList.add("navbar-items", "me-2", "mb-2");
+    categoryItem.innerHTML = `<a class="nav-link" onclick="loadCategoryNews('${category.category_id}')">${category.category_name}</a>`;
 
     categoriesContainer.appendChild(categoryItem);
-    });
+  });
 };
 // load news by categories
 const loadCategoryNews = async (id) => {
-    const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    displayCategoryNews(data.data);
-}
+  const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayCategoryNews(data.data);
+};
 // display category wise news
-const displayCategoryNews = categoryNews =>{
-    const newsContainer = document.getElementById('news-container');
-    newsContainer.textContent=``;
-    // short array short wise
-    categoryNews.sort(function (a,b){return b.total_view- a.total_view})
-    categoryNews.forEach(news=>{
-        console.log(news);
-    const newsDiv = document.createElement('div');
-    newsDiv.classList.add('card','mb-3');
-    newsDiv.innerHTML=
-    `<div class="row g-0 mb-2">
+const displayCategoryNews = (categoryNews) => {
+  const newsContainer = document.getElementById("news-container");
+  newsContainer.textContent = ``;
+  // short array short wise
+  categoryNews.sort(function (a, b) {
+    return b.total_view - a.total_view;
+  });
+  categoryNews.forEach((news) => {
+    // console.log(news);
+
+    // rating
+    const totalStars = 5;
+    const ratingNumber = `${news.rating.number}`;
+    // console.log(ratingNumber);
+
+    // get percentage value from rating Number
+    const starPercentage = (ratingNumber / totalStars) * 100;
+    // Rounded to nearest 10
+    const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+    // console.log(starPercentageRounded);
+    const newsDiv = document.createElement("div");
+    newsDiv.classList.add("card", "mb-3");
+    newsDiv.innerHTML = `<div class="row g-0 mb-2">
     <div class="col-md-3">
       <img src="${news.thumbnail_url}" class="img-fluid rounded-start p-3 ">
     </div>
     <div class="col-md-9 ">
       <div class="card-body">
         <h5 class="card-title">${news.title}</h5>
-        <p class="card-text">${news.details.slice(0,320)}...</p>
+        <p class="card-text">${news.details.slice(0, 320)}...</p>
 
       </div>
       <div class="d-flex mt-5 justify-content-between">
       <!--Author -->
         <div class="author d-flex">
-            <img src="${news.author.img}" class="rounded-circle me-2" style="height:70px";>
+            <img src="${
+              news.author.img
+            }" class="rounded-circle me-2" style="height:70px";>
             <div class="author-details">
-                <p>${news.author.name?news.author.name:"Unknown"}</p>
-                <p class="text-muted">${news.author.published_date.slice(0,10)}</p>
+                <p>${news.author.name ? news.author.name : "Unknown"}</p>
+                <p class="text-muted">${news.author.published_date.slice(
+                  0,
+                  10
+                )}</p>
             </div>
         </div>
       <!--Total View -->
@@ -58,12 +74,26 @@ const displayCategoryNews = categoryNews =>{
            <strong><i class="fa-regular fa-eye"></i></strong>
            <span>${news.total_view}</span>
            </div>
+      <!--Rating -->
+         <div>
+            <div class="stars-outer">
+              <div class="stars-inner">
+              </div>
+            </div>
+         </div>
+
        <div>
        </div>
       </div>
     </div>
    </div>`;
-   newsContainer.appendChild(newsDiv);
-    });
-}
+    //  append the news div to the news container
+    newsContainer.appendChild(newsDiv);
+    // Find the stars-inner element in the news div
+    const starsInner = newsDiv.querySelector(".stars-inner");
+
+    // Set the width of the stars-inner element to the starPercentageRounded value
+    starsInner.style.width = starPercentageRounded;
+  });
+};
 loadCategories();
